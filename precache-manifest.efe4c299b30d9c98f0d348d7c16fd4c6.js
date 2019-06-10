85b6dd173cfaa64c6359b814539b666762e17ba8
kraw3k <kacper5047@gmail.com>
2019-06-10 19:57:46 +0200

    Updates

---
+++ precache-manifest.efe4c299b30d9c98f0d348d7c16fd4c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cc9be84dfa39e6791ea",
+    "url": "/kraw3k/my-weight/static/js/runtime~main.2cc9be84.js"
+  },
+  {
+    "revision": "ac6386f84b1d66543784",
+    "url": "/kraw3k/my-weight/static/js/main.ac6386f8.chunk.js"
+  },
+  {
+    "revision": "340137a929aea551eccb",
+    "url": "/kraw3k/my-weight/static/js/1.340137a9.chunk.js"
+  },
+  {
+    "revision": "ac6386f84b1d66543784",
+    "url": "/kraw3k/my-weight/static/css/main.dc470f25.chunk.css"
+  },
+  {
+    "revision": "14a71ab24154e876206003e18bff0afa",
+    "url": "/kraw3k/my-weight/index.html"
+  }
+];
